7d4c766b1b1c9149c068ee3209933257ba15a774
Krishesh Shrestha <shrestha.krishesh@gmail.com>
2018-01-20 20:42:02 +0545

    Initial commit

---
+++ app/src/main/java/com/example/krishesh/todolist_version1/MemoEntry.java
@@ -0,0 +1,13 @@
+package com.example.krishesh.todolist_version1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MemoEntry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_memo_entry);
+    }
+}
